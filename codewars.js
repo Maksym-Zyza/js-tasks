38b52374179b28af56f36209bdab49e9e422fdf0
Maksym Zyza <mmziza88@gmail.com>
2021-09-25 13:25:47 +0300

    added new task

---
+++ codewars.js
@@ -0,0 +1,62 @@
+// // Function maskify, which changes all but the last four characters into '#'.
+
+// const code = 1234567890;
+
+// // function maskify(cc) {
+// //   const arr = String(cc).split("");
+
+// //   let sum = "";
+// //   for (let i = 0; i < arr.length - 4; i++) {
+// //     if (arr.length > 4) {
+// //       sum += "#";
+// //     }
+// //   }
+
+// //   const last = String(cc)
+// //     .split("")
+// //     .reverse()
+// //     .splice(0, 4)
+// //     .reverse()
+// //     .join("")
+// //     .toString();
+// //   // console.log(sum+last);
+// //   return sum + last;
+// // }
+
+// function maskify(cc) {
+//   console.log(
+//     String(cc).slice(0, -4).replace(/./g, "#") + String(cc).slice(-4)
+//   );
+// }
+
+// console.log(maskify(code));
+
+// const arr = [1, 10, 100, 1000, 10000, 5, 30, 2000, 30000];
+
+// function findLongest(arr) {
+//   let max = String(arr[0]).length;
+
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//       max = arr[i];
+//     }
+//   }
+//   return max;
+// }
+
+// console.log(findLongest(arr));
+
+const arr = [1, 10, 100, 1000, 10000, 5, 30, 2000, 30000];
+
+function findLongest(arr) {
+  let max = String(arr[0]).length;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+console.log(findLongest(arr));
--- new.js
+++ new.js
@@ -1,67 +1,55 @@
-const refs = {
-  input: document.querySelector(".content"),
-  button: document.querySelector("#todo-button-add"),
-};
+// Создание перемменних
+const st = new String();
+console.log(st);
 
-const todo = {
-  init() {
-    const fromStorage = localStorage.getItem("todo");
-    if (fromStorage) {
-      document.querySelector(".todo__items").innerHTML = fromStorage;
-    }
-    document.addEventListener("click", this.update.bind(this));
-    document.addEventListener("click", this.delete.bind(this));
-  },
+const obj = new Object();
+console.log(obj);
 
-  add() {
-    if (refs.input.disabled || !refs.input.value.length) {
-      return alert("Please enter text in the Todo field!");
-    }
-    document
-      .querySelector(".todo__items")
-      .insertAdjacentHTML("beforeend", this.create(refs.input.value));
-    refs.input.value = "";
-  },
+const arr = new Array();
+console.log(arr);
 
-  create(text) {
-    return `<li class="todo_item" data-todo="active">
-              <p class="todo_text">${text}</p>
-              <span class="todo_delete">Delete</span>
-            </li>`;
-  },
-
-  update(e) {
-    if (e?.target.tagName === "P") {
-      updeteText = prompt("Do you want to update Todo?");
-      if (updeteText) {
-        e.target.textContent = updeteText;
-        this.save();
-      }
-    }
-  },
-
-  save() {
-    localStorage.setItem(
-      "todo",
-      document.querySelector(".todo__items").innerHTML
-    );
-  },
-
-  delete(e) {
-    if (e?.target.tagName === "SPAN") {
-      e.target.parentNode.hidden = true;
-    }
-    this.save();
-  },
-};
-
-refs.button.addEventListener("click", handleTodos);
-
-todo.init();
-
-function handleTodos(e) {
-  todo.add();
-  todo.save();
-  todo.update();
-  todo.delete();
+//  rest - Синтаксис оставшихся параметров функции позволяет представлять неограниченное множество аргументов в виде массива.
+//https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Functions/rest_parameters
+function newPerson(name, ...rest) {
+  console.log(name);
+  return rest;
 }
+
+const bill = newPerson("Bill", 32, "tesla", "dog");
+console.log(bill);
+
+// Объекты Set представляют коллекции значений, по которым вы можете выполнить
+// обход в порядке вставки элементов. Значение элемента в Set может присутствовать
+// только в одном экземпляре, что обеспечивает его уникальность в коллекции Set.
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Set
+var arr1 = [1, 2, 3, 4, 5, 5, 4, 3, 3, 4, 5];
+var set = new Set(arr1);
+console.log(set);
+console.log(set.has(5));
+console.log(set.delete(5));
+console.log(set.size);
+console.log([...set]);
+
+// Об'єкт Map містить пари ключ-значення і запам'ятовує початковий порядок вставки ключів. Будь -яке значення (як об'єкти, так і примітивні значення) може використовуватися як ключ, так і значення.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map?retiredLocale=uk
+const map1 = new Map();
+
+map1.set("a", 1);
+map1.set("b", 2);
+map1.set("c", 3);
+
+console.log(map1.get("a"));
+// expected output: 1
+
+map1.set("a", 97);
+
+console.log(map1.get("a"));
+// expected output: 97
+
+console.log(map1.size);
+// expected output: 3
+
+map1.delete("b");
+
+console.log(map1.size);
+// expected output: 2
