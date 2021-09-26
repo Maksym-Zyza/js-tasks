9cd69ebdd92810750a986d590a357d28634d2b03
Maksym Zyza <mmziza88@gmail.com>
2021-09-26 17:17:15 +0300

    added new codewars task

--- codewars.js
+++ codewars.js
@@ -1,4 +1,4 @@
-// // Function maskify, which changes all but the last four characters into '#'.
+// ============ Function maskify, which changes all but the last four characters into '#'.
 
 // const code = 1234567890;
 
@@ -31,19 +31,45 @@
 
 // console.log(maskify(code));
 
-const arr = [1, 10, 100, 1000, 10000, 5, 30, 2000, 30000];
+// ============ Determining the largest number in the array
 
-function findLongest(arr) {
-  let max = String(arr[0]).length;
+// const arr = [1, 10, 100, 1000, 10000, 5, 30, 2000, 30000];
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > max) {
-      max = arr[i];
-    }
-  }
-  return max;
-}
+// function findLongest(arr) {
+//   let max = String(arr[0]).length;
 
-console.log(findLongest(arr));
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//       max = arr[i];
+//     }
+//   }
+//   return max;
+// }
 
-// my test
+// console.log(findLongest(arr));
+
+// ============ Definition of the first letter of the text in the alphabet
+
+// const string = "Z LKCQYEM WRPGNI UJ SER DVUD IFX R IPVUTU MCTYKTmx";
+
+// function connotation(str) {
+//   let arr = str.toLowerCase().split(" ");
+//   arr = arr.filter((el) => el.length > 0);
+//   const m = "m".charCodeAt();
+//   const num = arr.map((el) => (el.slice(0, 1).charCodeAt() < m ? 1 : 0));
+//   const sum = num.reduce((sum, a) => sum + a, 0);
+//   const half = Math.round(num.length / 2);
+//   const result = sum >= half;
+
+//   console.log(arr);
+//   console.log(arr.map((el) => el.slice(0, 1)));
+//   console.log(num);
+//   console.log(sum);
+//   console.log(half);
+//   console.log(result);
+//   return result;
+// }
+
+// connotation(string);
+
+// ============ Definition of the first letter of the text in the alphabet
